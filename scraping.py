e83f613a9bb23d603744488a22ee6647f8cca484
Anastasiia Matveeva <137638375+anastasmatv@users.noreply.github.com>
2023-06-29 16:15:59 +0200

    Create scraping.py

---
+++ scraping.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.example.com"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extract specific data from the webpage
+# ...
